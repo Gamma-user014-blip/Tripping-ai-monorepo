3b1ab0e492daded82f9deb6e669845d54076b3c8
Lior Gleizer <Gamma-user007@greendreamteam.onmicrosoft.com>
2026-01-24 23:08:33 +0200

    Revert "Final"
    This reverts commit 4592177552dcd45af8681889aeac725493b6d19f.

---
+++ apps/web-client/common/country-select/country-select-item.tsx
@@ -0,0 +1,16 @@
+import ReactCountryFlag from "react-country-flag";
+import styles from "./country-select-item.module.css";
+
+interface CountrySelectItemProps {
+  name: string;
+  code: string;
+}
+
+export const CountrySelectItem = ({ name, code }: CountrySelectItemProps) => {
+  return (
+    <div className={styles.item}>
+      <ReactCountryFlag countryCode={code} svg />
+      <span>{name}</span>
+    </div>
+  );
+};
